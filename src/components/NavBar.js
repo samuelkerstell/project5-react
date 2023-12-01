ca8ef68ef9a5f3d9be218ef0680fde197f2d0584
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-12-01 15:15:26 +0000

    added useContext hook to display a different navbar to logged in & out users

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,33 @@
-import React from 'react'
-import {Navbar, Container, Nav} from "react-bootstrap";
-import styles from '../styles/NavBar.module.css'
-import { NavLink } from "react-router-dom"
-// import logo from '../assets/logo.png'
+import React, { useContext } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext);
+
+  const loggedInIcons = <>{currentUser?.username}</>;
+
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fa-solid fa-right-to-bracket"></i>Sign in
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i className="fa-solid fa-user-plus"></i>Sign up
+      </NavLink>
+    </>
+  );
+  
   return (
     <Navbar className={styles.NavBar}  expand="md" fixed="top">
             <Container>
@@ -27,20 +50,7 @@ const NavBar = () => {
                         <i class="fa-solid fa-house"></i>
                         Home
                     </NavLink>
-                    <NavLink 
-                        className={styles.NavLink}
-                        activeClassName={styles.Active}
-                        to="/signin"
-                    >
-                        <i class="fa-solid fa-right-to-bracket"></i>Sign In
-                    </NavLink>
-                    <NavLink 
-                        className={styles.NavLink}
-                        activeClassName={styles.Active}
-                        to="signup"
-                    >
-                        <i class="fa-solid fa-user-plus"></i>Sign Up
-                    </NavLink>
+                    {currentUser ? loggedInIcons: loggedOutIcons}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import axios from "axios";
 
 import Form from "react-bootstrap/Form";
@@ -14,9 +14,12 @@ import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import { SetCurrentUserContext } from "../../App";
 
 
 function SignInForm() {
+  const setCurrentUser = useContext(SetCurrentUserContext);
+
     const [signInData, setSignInData] = useState({
         username: "",
         password: "",
@@ -37,7 +40,8 @@ function SignInForm() {
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-          await axios.post("/dj-rest-auth/login/", signInData);
+          const {data} = await axios.post("/dj-rest-auth/login/", signInData);
+          setCurrentUser(data.user)
           history.push("/");
         } catch (err) {
           setErrors(err.response?.data);
@@ -91,7 +95,7 @@ function SignInForm() {
                     className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} 
                     type="submit"
                 >
-                    Submit
+                    Sign In
                 </Button>
             </Form>
             {errors.non_field_errors?.map((message, idx) => (
