5deeba078ed2cf94da5ce89b61d47b4585f65796
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-12-04 18:46:53 +0000

    added InfiniteScroll with fetchMoreData function to keep loading more posts as we scroll

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -40,7 +40,7 @@ function PostPage() {
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
+        Recommended Profiles
       </Col>
     </Row>
   );
