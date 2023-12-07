2bdf5f9c2d917194d96027d7744aa7e6e47a3865
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-12-07 17:57:39 +0000

    added ProfileDataContext.js

--- src/pages/profiles/RecommendedProfiles.js
+++ src/pages/profiles/RecommendedProfiles.js
@@ -1,37 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Container } from "react-bootstrap";
-import { axiosReq } from "../../api/axiosDefaults";
 import appStyles from "../../App.module.css";
 import Asset from "../../components/Asset";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useProfileData } from "../../contexts/ProfileDataContext";
 import Profile from "./Profile";
 
 const RecommendedProfiles = ({ mobile }) => {
-  const [profileData, setProfileData] = useState({
-    // use for pageProfile later
-    pageProfile: { results: [] },
-    recommendedProfiles: { results: [] },
-  });
-  const {recommendedProfiles} = profileData;
-  const currentUser = useCurrentUser()
-
-  useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const { data } = await axiosReq.get(
-          "/profiles/?ordering=-followers_count"
-        );
-        setProfileData((prevState) => ({
-          ...prevState,
-          recommendedProfiles: data,
-        }));
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    handleMount();
-  }, [currentUser]);
+  const { popularProfiles } = useProfileData();
 
   return (
     <Container
@@ -41,7 +16,7 @@ const RecommendedProfiles = ({ mobile }) => {
     >
       {recommendedProfiles.results.length ? (
         <>
-          <p>Most followed profiles.</p>
+          <p>Recommended profiles.</p>
           {mobile ? (
             <div className="d-flex justify-content-around">
               {recommendedProfiles.results.slice(0, 4).map((profile) => (
