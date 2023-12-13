b62779380f6bfc9db653ac21ee60b735480c0da8
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-12-13 18:38:18 +0000

    handled refresh tokens errors

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,4 @@
+import jwtDecode from "jwt-decode";
 import { axiosReq } from "../api/axiosDefaults"
 
 export const fetchMoreData = async (resource, setResource) => {
@@ -46,3 +47,17 @@ export const followHelper = (profile, clickedProfile, following_id) => {
         // the user owns, so just return it unchanged
         profile;
   };
+
+  export const setTokenTimestamp = (data) => {
+    const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
+    localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
+  };
+  
+  export const shouldRefreshToken = () => {
+    return !!localStorage.getItem("refreshTokenTimestamp");
+  };
+  
+  export const removeTokenTimestamp = () => {
+    localStorage.removeItem("refreshTokenTimestamp");
+  };
+  
