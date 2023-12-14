5c4c773438545a3fd6956d58fdd45f193eeb1f7a
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-12-14 10:03:39 +0000

    update axios base url

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-axios.defaults.baseURL = 'project-portfolio-5-sam-7342558136fc.herokuapp.com'
+axios.defaults.baseURL = 'https://project-portfolio-5-sam-7342558136fc.herokuapp.com'
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
